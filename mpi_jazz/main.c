25cce388d297a0978cb9ece38fd7ffb9ecea9aaf
Nicholas Moran <Nicholas Moran>
2017-01-30 22:21:57 -0600

    mer

--- mpi_jazz/main.c
+++ mpi_jazz/main.c
@@ -1,13 +1,41 @@
-#include "mpi.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main( int argc, char *argv[])
+int main()
 {
-    int size, rank;
-    MPI_Init( &argc, &argv);
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
-    MPI_Comm_size( MPI_COMM_WORLD, &size);
-    printf("I am  %d or %d\n", rank, size);
-    MPI_Finalize();
+    int count = 0;
+    for(int i = 0; i < 100;  i++)
+    {
+        if (isHappy(i) == 1)
+        {
+            count++;
+        }
+    }
+    printf("This many %d\n", count);
     return 0;
 }
+
+int isHappy(int number) 
+{
+    char numberBank[15];
+    int total = 0;
+    int temp;
+    while (total != 4)
+    {
+        number = total;
+        sprintf(numberBank, "%d", number);
+        total = 0;
+        for (int i = 0; numberBank[i] != 0; i++)
+        {
+            temp = numberBank[i] - '0';
+            total += (temp * temp);
+        }
+        if (total == 1)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
