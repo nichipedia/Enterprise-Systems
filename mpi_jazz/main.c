6738b6c0bd6c8f24efada62375474bc3fd434c19
Nicholas <atreayou@gmail.com>
2017-02-01 19:33:27 -0600

    sieve is working now

--- mpi_jazz/main.c
+++ mpi_jazz/main.c
@@ -1,41 +1,77 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-#define LIMIT 200000
+#define MAX 2000000
 
-int * primeSiethe()
+/*
+ * Prints all primes less than MAX using the Sieve of Eratosthenes.
+ */
+
+unsigned * primeGenerator()
 {
-    int i,j;
-    int *primes;
-    int z = 1;
-    primes = malloc(sizeof(int)*LIMIT);
-    for (i=2;i<LIMIT;i++)
+    unsigned i, j;
+    unsigned * primes = malloc(sizeof(unsigned)*MAX);
+    for (unsigned i = 0; i < MAX; i++)
     {
-        primes[i]=1;
+        primes[i] = 1;
     }
-    for (i=2;i<LIMIT;i++)
+    for (i = 2; i<MAX; i++)
     {
-        if (primes[i]) 
+        j = i*i;
+        while(j<MAX)
         {
-            for (j=i;i*j<LIMIT;j++)
-            {
-                primes[i*j]=0;
-            }
+            primes[j] = 0;
+            j += i;
         }
     }
-    return primes;
+   return primes;
 }
 
+char *toCharBuffer(int number)
+{
+    int n = log10(number) + 1;
+    char *buffer = malloc(sizeof(char)*n);
+    sprintf(buffer, "%d", number);
+    return buffer;
+}
+
+int isHappy(int number)
+{
+    int seq, i;
+    while(seq != 4)
+    {
+        char *buffer = toCharBuffer(number);
+        for (i, seq = 0; i < strlen(buffer); i++)
+        {
+            seq += pow(buffer[i], 2); 
+        }
+        free(buffer);
+        if(seq == 1)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main()
 {
-   int *primes = primeSiethe();
-   for (int i = 0; i < 200000; i++)
-   {
-       if (primes[i])
-       {
-           printf("I am %d and I am prime\n", i);
-       }
-   }
+    //unsigned * primes = primeGenerator();
+    if(isHappy(6))
+    {
+        printf("yas\n");
+    }
+    else 
+    {
+        printf("oh boi\n");
+    }
     return 0;
 }
+
+
+
+
+
+
